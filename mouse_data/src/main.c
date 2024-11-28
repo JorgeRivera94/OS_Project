7eaa08f04a8e2511055242e407b401190f1f4b23
JorgeRivera94 <jrivera28901@gmail.com>
2024-11-28 02:47:02 +0000

    converts absolute coordinates into virtual

--- mouse_data/src/main.c
+++ mouse_data/src/main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 int main() {
-  const char *device = "/dev/input/mice";  // Generic mouse device file
+  const char *device = "/dev/input/mice"; // Generic mouse device file
   int fd = open(device, O_RDONLY);
   if (fd == -1) {
     perror("Error opening device");
@@ -14,6 +14,12 @@ int main() {
   signed char data[3];
 
   printf("Listening for mouse events. Move the mouse or press buttons.\n");
+
+  int sx = 0;
+  int sy = 0;
+  double vx;
+  double vy;
+
   while (1) {
     if (read(fd, data, sizeof(data)) > 0) {
       int left = data[0] & 0x1;
@@ -22,8 +28,19 @@ int main() {
       int dx = data[1];
       int dy = data[2];
 
-      printf("Left: %d, Right: %d, Middle: %d, dx: %d, dy: %d\n", left, right,
-             middle, dx, dy);
+      // Capture the absolute x coordinate by storing the sumation of all dx
+      sx += dx;
+
+      // Capture the absolute y coordinate by storing the sumation of all dy
+      sy += dy;
+
+      // Transform the absolute x coordinates to a [0,100] range
+      vx = (100.0 / 1023.0) * (sx + 512.0);
+
+      // Transform the absolute y coordinates to a [0,25] range
+      vy = (25.0 / 767.0) * (sy + 383.0);
+
+      printf("sx: %d, sy: %d, vx: %f, vy: %f\n", sx, sy, vx, vy);
     }
   }
 
